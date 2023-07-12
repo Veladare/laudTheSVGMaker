e19f36787fca2b357e1463180a119a9c392d332b
Jean-Luc <jeanlucv@berkeley.edu>
2023-07-11 22:27:14 -0700

    Still need to add examples and readme but done

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const { Triangle, Square, Circle } = require("./lib/shapes");
-
-class SVG {
-  constructor(textElement, shapeElement) {
-    this.textElement = textElement;
-    this.shapeElement = shapeElement;
-  }
-
-  render() {
-    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
-  }
-
-  setTextElement(color, text) {
-    this.textElement = `<text x="150" y="150" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
-  }
-
-  setShapeElement(shape) {
-    this.shapeElement = shape.render();
-  }
-}
+const SVG  = require("./lib/svgfile")
 
 const questions = [
   {
@@ -86,5 +68,3 @@ function init() {
 }
 
 init();
-
-module.exports = {SVG, init}
